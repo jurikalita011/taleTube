9c36122037a761bbee7190992f225f6af00a576e
Nishal Barman <32856353+nishalbarman@users.noreply.github.com>
2024-04-09 10:55:41 +0530

    bug fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,18 @@
+import ProtectedRoute from "@/components/ProtectedRoute/ProtectedRoute";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import React from "react";
 
 async function page() {
   return (
-    <div>
-      I am the dashboard, if you are able to access this page that means either
-      authorization is given or may be app is broken.
-    </div>
+    <ProtectedRoute roleLevel="adfa">
+      <div>
+        I am the dashboard, if you are able to access this page that means
+        either authorization is given or may be app is broken.
+      </div>
+    </ProtectedRoute>
   );
 }
 
 export default withPageAuthRequired(page, {
-  returnTo: "/profile",
+  returnTo: "/dashboard",
 });
